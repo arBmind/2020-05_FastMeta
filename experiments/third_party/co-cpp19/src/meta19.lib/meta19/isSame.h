9ea560c732a65f0e0f41e45f69c4d6ab0e5ee434
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-05-17 20:51:36 +0200

    simplified error detection for isSame

--- experiments/third_party/co-cpp19/src/meta19.lib/meta19/isSame.h
+++ experiments/third_party/co-cpp19/src/meta19.lib/meta19/isSame.h
@@ -1,13 +1,12 @@
 #pragma once
-#include "false_for.h"
 
 namespace meta19 {
 
 constexpr bool isSame(const void* a, const void* b) { return a == b; }
 
-template<class T, class U> //
-constexpr auto isSame(T*&, U*&) {
-    static_assert(false_for<T, U>, "HINT: use &type<T>!");
+template<class T, class U> constexpr bool isSame(T*&, U*&) {
+    static_assert((sizeof(T), sizeof(U), false), "HINT: use &type<T>!");
+    return false;
 }
 
 } // namespace meta19
